fb8a0009bef7a716349cade8ed4e30ada533bc99
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-10-21 13:49:03 -0300

    feat: Mudança nos flash-messages. *-*

---
+++ resources/assets/js/toast.js
@@ -0,0 +1,72 @@
+(function ($) {
+  'use strict';
+
+  // Caso a tela seja muito pequena:
+  if (window.innerWidth < 768) {
+    return;
+  }
+
+  /**
+   * Cria uma barrinha de contagem.
+   * 
+   * @param  {object <jQuery Element>} $context 
+   * @return {object<jQueryElement>}
+   */
+  function createBar ($context) {
+    var $bar = $('<span>', {
+      'class': 'sys-toast-bar'
+    })
+      .css('width', '100%');
+
+    $bar.appendTo($context);
+    return $bar;
+  }
+
+  function animateBar ($toast, $bar, duration) {
+    $bar.animate({
+      width: '0%'
+    }, {
+      duration: duration,
+      easing: 'linear',
+      complete: function () {
+        $toast.addClass('--hidden');
+      }
+    });
+  }
+
+  $(function () {
+    $('.sys-toast').each(function () {
+      var $this = $(this);
+
+      if ($this.is(':hidden')) {
+        return;
+      }
+    
+      var text = $this.text().trim();
+      var words = text.split(/\s/g).length;
+      var time = (words / 3.33) >= 7 ? words / 3.25 : 7;
+
+      var $bar = createBar($this);
+      animateBar($this, $bar, (time * 1000));
+
+      $this.on('mouseenter', function () {
+        if ($this.is(':hidden')) {
+          return;
+        }
+
+        $bar.stop();
+        $bar.fadeOut(150);
+      });
+
+      $this.on('mouseleave', function () {
+        $bar = createBar($this);
+        animateBar($this, $bar, 1250);
+      });
+
+      $this.on('click', function () {
+        $bar.stop();
+        $this.addClass('--hidden');
+      });
+    });
+  });
+})(jQuery);
