2433c70ce611c88bb3e5a4607bb6561d96455d31
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-10-21 12:11:28 -0300

    feat: Hash nav. *-*

---
+++ resources/assets/js/hash-nav.js
@@ -0,0 +1,19 @@
+(function ($) {
+  'use strict';
+
+  if (!/^#\$tab\/(.+)$/i.test(location.hash)) {
+    return;
+  }
+
+  var hash = location.hash.replace(/^#\$tab\/(.+)$/i, '$1');
+
+  $(function () {
+    var $tab = $('[data-toggle="tab"][href="#' + hash + '"]');
+
+    if (!$tab.length) {
+      return;
+    }
+
+    $tab.tab('show');
+  });
+})(jQuery);
