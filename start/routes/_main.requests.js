8f6d514f7fd7d9b4106da11c3475c29086fd90c6
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-10-25 23:25:57 -0300

    feat: Criação das rotas para a interface das requisições e reviews.

--- start/routes/_main.requests.js
+++ start/routes/_main.requests.js
@@ -10,11 +10,23 @@ const Route = use('Route')
 
 Route.group(() => {
   Route.get('/', 'RequestController.index').as('requests.index')
-  Route.get('list/:slug', 'RequestController.list').as('requests.list')
+  Route.get('all', 'RequestController.all').as('requests.all')
+  Route.get('search', 'RequestController.search').as('requests.search')
 })
   .middleware(['auth'])
   .namespace('Main')
   .prefix('crh')
+
+Route.group(() => {
+  Route.get(':id', 'RequestInstanceController.show').as('requests.show')
+  Route.post(':id/comment', 'RequestInstanceController.comment').as('requests.comment')
+
+  Route.get(':id/edit', 'RequestInstanceController.edit').as('requests.edit')
+  Route.post(':id/edit', 'RequestInstanceController.update')
+
+  Route.get(':id/review', 'RequestInstanceController.review').as('requests.review')
+  Route.post(':id/review', 'RequestInstanceController.review')
+})
 
 /**
  * ---------------------------------------------------------------------
@@ -35,7 +47,7 @@ Route.group(() => {
 })
   .middleware(['auth'])
   .namespace('Main')
-  .prefix('crh/requests/create')
+  .prefix('crh/requests/new')
 
 const RequestType = use('App/Models/RequestType')
 Route.get('request-types', async ({ view }) => {
