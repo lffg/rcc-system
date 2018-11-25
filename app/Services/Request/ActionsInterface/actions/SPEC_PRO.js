5c4fe60d4dfaee540085f4476a39bfb1002101be
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-25 20:19:41 -0200

    feat: Action de promoção.

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_PRO.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_PRO
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: false,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const user = await User.findOrFail(request.receiver_id)
+  user.promoter_id = request.author_id
+  user.position_id = request.after_position_id
+  user.tag_type = 'NORMAL'
+  user.change_position_date = new Date()
+  await user.save(transaction)
+}
