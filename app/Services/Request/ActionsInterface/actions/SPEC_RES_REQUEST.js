b7f6091a51a919487dd269adbf35b33950090fbe
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-25 20:54:49 -0200

    feat: Action de reserva.

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_RES_REQUEST.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_RES_REQUEST
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: false,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const user = await User.findOrFail(request.receiver_id)
+  user.absence_until = request.absence_until
+  await user.save(transaction)
+}
