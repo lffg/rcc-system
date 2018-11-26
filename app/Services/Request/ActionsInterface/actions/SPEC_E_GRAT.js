73f6cd271aef70e110984d74e1a6323c7feb6958
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-26 16:32:25 -0200

    feat: Actions de grat T e E.

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_E_GRAT.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_E_GRAT
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: false,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const user = await User.findOrFail(request.receiver_id)
+  user.effective_bonuses = parseInt(user.effective_bonuses, 10) + parseInt(request.bonuses, 10)
+  await user.save(transaction)
+}
---
+++ app/Services/Request/ActionsInterface/actions/SPEC_T_GRAT.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_T_GRAT
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: false,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const user = await User.findOrFail(request.receiver_id)
+  user.temporary_bonuses = parseInt(user.temporary_bonuses, 10) + parseInt(request.bonuses, 10)
+  await user.save(transaction)
+}
