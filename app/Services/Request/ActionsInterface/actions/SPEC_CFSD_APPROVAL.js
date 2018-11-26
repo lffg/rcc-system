57eb09bcfcbfe4545b6e194fe53ea6738704b0b9
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-25 22:08:36 -0200

    feat: action de aprovação em cfsd

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_CFSD_APPROVAL.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_CFSD_APPROVAL
+ */
+
+const Position = use('App/Models/Position')
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: true,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const position = await Position.findByOrFail('alias', 'SLD')
+  const user = await User.findOrFail(request.receiver_id)
+
+  user.state = 'ACTIVE'
+  user.position_id = position.id
+  user.promoter_id = request.author_id
+  user.tag_type = 'NORMAL'
+  user.change_position_date = new Date()
+  await user.save(transaction)
+}
