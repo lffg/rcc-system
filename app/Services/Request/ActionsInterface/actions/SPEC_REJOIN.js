646a9abff9eba2d0ec552d0b4e0a31017364214b
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-26 15:40:43 -0200

    teste

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_REJOIN.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_REJOIN
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: true,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, authUser, request, payload }) {
+
+}
