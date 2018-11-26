ba9143aee9b884de1b454047fd493df850d11f65
Luiz Felipe <luizfelipefrazaogoncalves@gmail.com>
2018-11-25 22:00:39 -0200

    feat: Action de compra de cargo.

---
+++ app/Services/Request/ActionsInterface/actions/SPEC_BUY_POS.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * Action usada para:
+ *    - Especialização em Requerimento :: SPEC_BUY_POS
+ */
+
+const User = use('App/Models/User')
+
+module.exports = () => ({
+  requiresTransaction: true,
+  requiresController: false,
+  requiresAuthUser: false,
+  requiresRequest: true,
+  requiresReview: false,
+  requiresType: false,
+  caller
+})
+
+async function caller ({ transaction, request }) {
+  const user = await User.findOrFail(request.receiver_id)
+  user.position_id = request.after_position_id
+  user.promoter_id = request.author_id
+  user.tag_type = 'NORMAL'
+  await user.save(transaction)
+}
